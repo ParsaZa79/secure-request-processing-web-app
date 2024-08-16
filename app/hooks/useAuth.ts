058378a699c4dbe1a2024054b37eb78729f019b9
Parsa Zeinali <pzeinali4@gmail.com>
2024-08-16 18:35:47 +0330

    chore: Update useAuth hook to handle Google and GitHub login

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -1,3 +1,4 @@
+// @useAuth.ts
 import { useState, useEffect, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import jwt from "jsonwebtoken";
@@ -5,66 +6,100 @@ import jwt from "jsonwebtoken";
 import { useAppStore } from "../store";
 
 interface DecodedToken {
+  id: number;
+  username: string;
   email: string;
   name: string;
   picture: string;
-  exp: number;
 }
 
 export function useAuth() {
   const [token, setToken] = useState<string | null>(null);
   const [user, setUser] = useState<DecodedToken | null>(null);
   const [loading, setLoading] = useState(true);
-  const loginFromStore = useAppStore((state) => state.login);
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+  const { googleLogin: googleLoginStore, githubLogin: githubLoginStore } =
+    useAppStore();
 
   const router = useRouter();
 
-  const checkAuth = useCallback(() => {
+  const updateAuthState = useCallback(() => {
     const storedToken = localStorage.getItem("sessionToken");
 
+    console.log("Stored token:", storedToken);
 
     if (storedToken) {
       try {
         const decoded = jwt.decode(storedToken) as DecodedToken;
 
+        console.log("Decoded token:", decoded);
+
         if (decoded && decoded.exp * 1000 > Date.now()) {
           setToken(storedToken);
           setUser(decoded);
-        } else {
-          logout();
+
+          return true;
         }
       } catch (error) {
-        console.error("Failed to decode token:", error);
-        logout();
+        // console.error("Failed to decode token:", error);
       }
     }
-    setLoading(false);
+
+    return false;
   }, []);
+
+  const checkAuth = useCallback(() => {
+    if (updateAuthState()) {
+      router.push("/dashboard");
+    }
+    setLoading(false);
+  }, [updateAuthState, router]);
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  const login = async (code: string) => {
-    try {
-      await loginFromStore(code);
-
-      const storedToken = localStorage.getItem("sessionToken");
-
-      if (storedToken) {
-        const decoded = jwt.decode(storedToken) as DecodedToken;
-
-        setToken(storedToken);
-        setUser(decoded);
-        router.push("/dashboard");
-      } else {
-        throw new Error("Failed to get session token");
+  const googleLogin = useCallback(
+    async (code: string) => {
+      if (isLoggingIn) return;
+      setIsLoggingIn(true);
+      try {
+        await googleLoginStore(code);
+        if (updateAuthState()) {
+          router.push("/dashboard");
+        } else {
+          throw new Error("Failed to update auth state");
+        }
+      } catch (error) {
+        console.error("Google login failed:", error);
+        throw error;
+      } finally {
+        setIsLoggingIn(false);
       }
-    } catch (error) {
-      console.error("Login failed:", error);
-      throw error;
-    }
-  };
+    },
+    [isLoggingIn, googleLoginStore, updateAuthState, router],
+  );
+
+  const githubLogin = useCallback(
+    async (code: string) => {
+      if (isLoggingIn) return;
+      setIsLoggingIn(true);
+      try {
+        await githubLoginStore(code);
+        if (updateAuthState()) {
+          router.push("/dashboard");
+        } else {
+          throw new Error("Failed to update auth state");
+        }
+      } catch (error) {
+        // console.error("GitHub login failed:", error);
+        throw error;
+      } finally {
+        setIsLoggingIn(false);
+      }
+    },
+    [isLoggingIn, githubLoginStore, updateAuthState, router],
+  );
 
   const logout = useCallback(() => {
     setToken(null);
@@ -73,5 +108,5 @@ export function useAuth() {
     router.push("/login");
   }, [router]);
 
-  return { token, user, login, logout, loading, checkAuth };
+  return { token, user, googleLogin, githubLogin, logout, loading, checkAuth };
 }
